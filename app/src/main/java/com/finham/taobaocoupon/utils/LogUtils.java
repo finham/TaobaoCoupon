500f2af61c1077869205a4b69da8de3966ee68ed
finham <1942666318@qq.com>
2020-04-14 20:28:23 +0800

    10 重构LogUtils

--- app/src/main/java/com/finham/taobaocoupon/utils/LogUtils.java
+++ app/src/main/java/com/finham/taobaocoupon/utils/LogUtils.java
@@ -8,32 +8,31 @@ import android.util.Log;
  * Time: 20:05
  */
 public class LogUtils {
-    public static final int verboseLevel = 5;
-    public static final int debugLevel = 4;
-    public static final int infoLevel = 3;
-    public static final int warningLevel = 2;
-    public static final int errorLevel = 1;
-    public static final int nothingLevel = 0;
-    public static int currentLevel = verboseLevel;
+    private static final int VERBOSE_LEVEL = 5;
+    private static final int DEBUG_LEVEL = 4;
+    private static final int INFO_LEVEL = 3;
+    private static final int WARNING_LEVEL = 2;
+    private static final int ERROR_LEVEL = 1;
+    private static int currentLevel = VERBOSE_LEVEL;
 
     public static void v(Class clazz, String log) {
-        if (currentLevel >= verboseLevel) Log.v(clazz.getSimpleName(), log);
+        if (currentLevel >= VERBOSE_LEVEL) Log.v(clazz.getSimpleName(), log);
     }
 
     public static void d(Class clazz, String log) {
-        if (currentLevel >= debugLevel) Log.d(clazz.getSimpleName(), log);
+        if (currentLevel >= DEBUG_LEVEL) Log.d(clazz.getSimpleName(), log);
     }
 
     public static void i(Class clazz, String log) {
-        if (currentLevel >= infoLevel) Log.i(clazz.getSimpleName(), log);
+        if (currentLevel >= INFO_LEVEL) Log.i(clazz.getSimpleName(), log);
     }
 
     public static void w(Class clazz, String log) {
-        if (currentLevel >= warningLevel) Log.w(clazz.getSimpleName(), log);
+        if (currentLevel >= WARNING_LEVEL) Log.w(clazz.getSimpleName(), log);
     }
 
     public static void e(Class clazz, String log) {
-        if (currentLevel >= errorLevel) Log.e(clazz.getSimpleName(), log);
+        if (currentLevel >= ERROR_LEVEL) Log.e(clazz.getSimpleName(), log);
     }
 
 }
