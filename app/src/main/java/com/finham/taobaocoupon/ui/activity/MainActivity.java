82d3e089d19d9b3a1bb3e469b3dddae090a4d60f
finham <1942666318@qq.com>
2020-04-14 21:11:42 +0800

    11 四个Fragment实现主页面切换

--- app/src/main/java/com/finham/taobaocoupon/ui/activity/MainActivity.java
+++ app/src/main/java/com/finham/taobaocoupon/ui/activity/MainActivity.java
@@ -9,8 +9,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.finham.taobaocoupon.R;
+import com.finham.taobaocoupon.base.BaseFragment;
 import com.finham.taobaocoupon.ui.fragment.HomeFragment;
-import com.finham.taobaocoupon.utils.LogUtils;
+import com.finham.taobaocoupon.ui.fragment.PocketFragment;
+import com.finham.taobaocoupon.ui.fragment.SearchFragment;
+import com.finham.taobaocoupon.ui.fragment.SelectedFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
@@ -18,38 +21,44 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
-    @BindView(R.id.main_page_container)
-    BottomNavigationView bottom;
+    @BindView(R.id.main_navigation_view) //黄油刀不能用来布局变量中，且不能为private或static，因为使用了反射
+            BottomNavigationView bottom;
+    private HomeFragment homeFragment;
+    SelectedFragment selectedFragment;
+    PocketFragment pocketFragment;
+    SearchFragment searchFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        //bottom = findViewById(R.id.main_navigation_view); //黄油刀不能用来布局变量中，且不能为private或static，因为使用了反射
-        bottom.setOnNavigationItemSelectedListener(this);
 
-        //可以使用Navigation来做！
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.main_page_container, homeFragment);
-        transaction.commit();
+        homeFragment = new HomeFragment();
+        selectedFragment = new SelectedFragment();
+        pocketFragment = new PocketFragment();
+        searchFragment = new SearchFragment();
+
+        bottom.setOnNavigationItemSelectedListener(this);
+        bottom.setSelectedItemId(R.id.home);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         //等Fragment的事务使用熟练了，再去使用Navigation！
-//        Log.d(TAG, "title -->" + item.getTitle() + "id -->" + item.getItemId());
-        if (item.getItemId() == R.id.home) {  //menu上的id不会智能提示，要自己手打完整
-            LogUtils.v(MainActivity.class,"首页");
-        } else if (item.getItemId() == R.id.selected) {
-            LogUtils.v(MainActivity.class,"精选");
-        } else if (item.getItemId() == R.id.pocket) {
-            LogUtils.v(MainActivity.class,"特惠");
-        } else if (item.getItemId() == R.id.search) {
-            LogUtils.v(MainActivity.class,"搜索");
-        }
+        //menu上的id不会智能提示，要自己手打完整
+        //LogUtils使用方法：LogUtils.v(MainActivity.class, "首页");
+        if (item.getItemId() == R.id.home) switchToFragment(homeFragment);
+        else if (item.getItemId() == R.id.selected) switchToFragment(selectedFragment);
+        else if (item.getItemId() == R.id.pocket) switchToFragment(pocketFragment);
+        else if (item.getItemId() == R.id.search) switchToFragment(searchFragment);
         return true; //return true表示消费这个事件，图标才会真正跟着切换过去
+    }
+
+    private void switchToFragment(BaseFragment target) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.main_page_container, target);
+        transaction.commit();
     }
 }
