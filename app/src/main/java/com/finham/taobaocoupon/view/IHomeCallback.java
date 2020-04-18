bc1ec4eac54c00321f8a0d9f5bbf3d99a095f6e3
finham <1942666318@qq.com>
2020-04-18 17:10:51 +0800

    17 修改TabLayout样式

--- app/src/main/java/com/finham/taobaocoupon/view/IHomeCallback.java
+++ app/src/main/java/com/finham/taobaocoupon/view/IHomeCallback.java
@@ -7,7 +7,7 @@ import com.finham.taobaocoupon.model.domain.Category;
  * Date: 2020/4/17
  * Time: 11:37
  */
-//当从网络取到数据时，需要通知UI进行更新
+//当从网络取到数据时，需要通知UI进行更新，这里的接口与方法都是跟UI相关的
 public interface IHomeCallback {
     void onCategoriesLoaded(Category category);
 }
