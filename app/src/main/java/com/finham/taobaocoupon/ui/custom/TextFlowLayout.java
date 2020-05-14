451b81d8a0674ad19e9ec3d799d68a659b0832ae
finham <1942666318@qq.com>
2020-05-14 15:44:31 +0800

    91 编写TextFlowLayout二

--- app/src/main/java/com/finham/taobaocoupon/ui/custom/TextFlowLayout.java
+++ app/src/main/java/com/finham/taobaocoupon/ui/custom/TextFlowLayout.java
@@ -1,6 +1,7 @@
 package com.finham.taobaocoupon.ui.custom;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -17,31 +18,55 @@ import java.util.List;
  */
 public class TextFlowLayout extends ViewGroup {
     private List<String> mTextList;
+    private float mItemHorizontalSpace = 10;
+    private float mItemVerticalSpace = 10;
 
+    //暴露出去给外面设置
+    public float getItemHorizontalSpace() {
+        return mItemHorizontalSpace;
+    }
+
+    public void setItemHorizontalSpace(float itemHorizontalSpace) {
+        mItemHorizontalSpace = itemHorizontalSpace;
+    }
+
+    public float getItemVerticalSpace() {
+        return mItemVerticalSpace;
+    }
+
+    public void setItemVerticalSpace(float itemVerticalSpace) {
+        mItemVerticalSpace = itemVerticalSpace;
+    }
+
+    //三个构造方法统一一个入口，一般统一为有三个参数的构造方法
     public TextFlowLayout(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public TextFlowLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public TextFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.FlowTextStyle);
+        mItemHorizontalSpace = array.getDimension(R.styleable.FlowTextStyle_horizontalSpace, 10);
+        mItemVerticalSpace = array.getDimension(R.styleable.FlowTextStyle_verticalSpace, 10);
+        array.recycle();
     }
 
-    public void setTextList(List<String> textList){
+    public void setTextList(List<String> textList) {
         this.mTextList = textList;
         //遍历
         for (String s : mTextList) {
             //TextView textView = new TextView(getContext());  //也可以使用xml来写。推荐现阶段还是使用XML来写
             //LayoutInflater.from(getContext()).inflate(R.layout.flow_text_view, this, true) 等价于下面两句，注意第三个参数是否黏贴到root中
-            TextView item = (TextView)LayoutInflater.from(getContext()).inflate(R.layout.flow_text_view, this, false);
+            TextView item = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.flow_text_view, this, false);
             item.setText("电脑");
             addView(item);
         }
     }
-    
+
     @Override
     protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
 
